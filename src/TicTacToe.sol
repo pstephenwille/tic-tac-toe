a56fcad474e54f3e6c246e5b009c7c3582fa93d0
stephen.wille <stephen.wille@climate.com>
2024-08-18 20:44:48 -0700

    flat game board

--- src/TicTacToe.sol
+++ src/TicTacToe.sol
@@ -8,71 +8,70 @@ import {GameEvent} from "./GameEvent.sol";
 
 contract TicTacToe is GameEvent {
     address owner;
-    uint8[3][3] public gameBoard;
+    uint8[9] public gameBoard;
     string public nonce = 'default';
-    uint8[2] playersLastMove;
+    uint8 playersLastMove;
     uint8 constant BOT = 0;
     uint8 constant PLAYER = 1;
+    uint8 constant EMPTY_SPACE = 10;
     uint8 public moveCounter = 0;
 
     struct Winner {
         uint8 player;
-        uint8[2][3] positions;
+        uint8[3] positions;
     }
 
     Winner public winner;
 
     constructor() {
         owner = msg.sender;
-        initGameBoard();
     }
 
     function initGameBoard() public {
-        gameBoard = [[10, 10, 10], [10, 10, 10], [10, 10, 10]];
+        gameBoard = [EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE,
+                    EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE,
+                    EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE];
         emitGameEvent("new game board created");
     }
 
-    function makePlayerXMove(uint8 _x, uint8 _y, string calldata _nonce) public {
+    function makePlayerMove(uint8 _pos, string calldata _nonce) public {
 //        require(nonce != null, "nonce value is required");
+        require(isMoveLegal(_pos), "move not allowed");
         nonce = _nonce;
         moveCounter++;
-        playersLastMove[0] = _x;
-        playersLastMove[1] = _y;
-        gameBoard[_x][_y] = PLAYER;
-
-        emitMoveMadeEvent(_x, _y, PLAYER);
+        playersLastMove = _pos;
+        gameBoard[_pos] = PLAYER;
+//        emitMoveMadeEvent(_pos, PLAYER);
 
         makeBotMove();
 
         bool winnerFound = _checkBoardForWinner();
-
-        if(winnerFound){
-            emitGameOverEvent(winnerFound, winner.player, winner.positions);
+        if (winnerFound) {
+//            emitGameOverEvent(winnerFound, winner.player, winner.positions);
         }
+
+        bool isDraw = _gameIsADraw();
+        if(isDraw){
+            //emitGameOverEvent()
+        }
+
     }
 
 
     function makeBotMove() public {
-        (uint8 botPosX, uint8 botPosY) = _generateRandomXYPoint();
+        uint8 botPos = _generateRandomXYPoint();
         bool isMoveLegal = false;
         while (!isMoveLegal) {
-            isMoveLegal = isPlannedMoveLegal(botPosX, botPosY);
-            botPosX = botPosX++ % 3;
-            botPosY = botPosY++ % 3;
+            isMoveLegal = isMoveLegal(botPos);
+            botPos++;
         }
-        gameBoard[botPosX][botPosY] = BOT;
+        gameBoard[botPos] = BOT;
 
-//        string memory botX = Strings.toString(botPosX);
-//        string memory botY = Strings.toString(botPosY);
-//        string memory message = string.concat("Bot moved to", " ", botX, ", ", botY);
-//        emitGameEvent(message);
-
-        emitMoveMadeEvent(botPosX, botPosY, BOT);
+//        emitMoveMadeEvent(botPosX, botPosY, BOT);
     }
 
     function _checkBoardForWinner() public returns (bool){
 //        require(moveCounter >= 3, "too few moves to win game");
-
         if (_findColumnWinner()) return true;
         if (_findRowWinner()) return true;
         if (_findDiagonalWinner()) return true;
@@ -80,53 +79,50 @@ contract TicTacToe is GameEvent {
         return false;
     }
 
+    function _gameIsADraw() public {
+        for (uint8 i = 0; i < gameBoard.length; i++) {
+            if (gameBoard[i] == EMPTY_SPACE) return false;
+        }
+
+        return true;
+    }
+
     function _findDiagonalWinner() public returns (bool)  {
-        uint8 a = gameBoard[0][0];
-        uint8 b = gameBoard[1][1];
-        uint8 c = gameBoard[2][2];
+        //0,4,8; 2,4,6
+        if (gameBoard[4] == EMPTY_SPACE) return false;
 
-        if (a != 10) {
-            if (a == b && a == c) {
-                winner.player = a;
-                winner.positions[0] = [0, 0];
-                winner.positions[1] = [1, 1];
-                winner.positions[2] = [2, 2];
+        if (gameBoard[4] == gameBoard[0] && gameBoard[4] == gameBoard[8]) {
+            winner.player = gameBoard[4];
+            winner.positions[0] = gameBoard[0];
+            winner.positions[1] = gameBoard[4];
+            winner.positions[2] = gameBoard[8];
 
-                return true;
-            }
+            return true;
         }
+        if (gameBoard[4] == gameBoard[2] && gameBoard[4] == gameBoard[6]) {
+            winner.player = gameBoard[4];
+            winner.positions[0] = gameBoard[2];
+            winner.positions[1] = gameBoard[4];
+            winner.positions[2] = gameBoard[6];
 
-        uint8 aa = gameBoard[0][2];
-        uint8 bb = gameBoard[1][1];
-        uint8 cc = gameBoard[2][0];
-
-        if (aa != 10) {
-            if (aa == bb && aa == cc) {
-                winner.player = a;
-                winner.positions[0] = [0, 2];
-                winner.positions[1] = [1, 1];
-                winner.positions[2] = [2, 0];
-
-                return true;
-            }
+            return true;
         }
 
         return false;
     }
 
-    function _findColumnWinner() internal returns (bool){
-        for (uint8 col = 0; col < gameBoard.length; col++) {
-            uint8 a = gameBoard[0][col];
-            uint8 b = gameBoard[1][col];
-            uint8 c = gameBoard[2][col];
+    function _findColumnWinner() public returns (bool){
+        //1,4,7; 2,5,8; 3,6,9;
+        //0,3,6; 1,4,7; 2,5,8;
+        for (uint8 col = 0; col < 3; col++) {
+            if (gameBoard[col] == EMPTY_SPACE) continue;
 
-            if (a == 10) continue;
-
-            if (a == b && b == c) {
-                winner.player = a;
-                winner.positions[0] = [0, col];
-                winner.positions[1] = [1, col];
-                winner.positions[2] = [2, col];
+            if (gameBoard[col] == gameBoard[col + 3] &&
+                gameBoard[col] == gameBoard[col + 6]) {
+                winner.player = gameBoard[col];
+                winner.positions[0] = col;
+                winner.positions[0] = col + 3;
+                winner.positions[0] = col + 6;
 
                 return true;
             }
@@ -135,45 +131,35 @@ contract TicTacToe is GameEvent {
         return false;
     }
 
-    function _findRowWinner() internal returns (bool){
-        for (uint8 row = 0; row < gameBoard.length; row++) {
-            uint8 a = gameBoard[row][0];
-            uint8 b = gameBoard[row][1];
-            uint8 c = gameBoard[row][2];
+    function _findRowWinner() public returns (bool){
+        //0,1,2; 3,4,5;, 6,7,8;
+        for (uint8 row = 0; row < 3; row++) {
+            if (gameBoard[row] == EMPTY_SPACE) continue;
+            if (gameBoard[row] == gameBoard[row + 1] &&
+                gameBoard[row] == gameBoard[row + 2]) {
 
-            if (a == 10) continue;
-
-            if (a == b && b == c) {
-                winner.player = a;
-                winner.positions[0] = [row, 0];
-                winner.positions[1] = [row, 1];
-                winner.positions[2] = [row, 2];
+                winner.player = gameBoard[row];
+                winner.positions[0] = gameBoard[row];
+                winner.positions[1] = gameBoard[row + 1];
+                winner.positions[2] = gameBoard[row + 2];
 
                 return true;
             }
-        }
 
+        }
         return false;
     }
 
-    function _generateRandomXYPoint() internal returns (uint8, uint8) {
-        uint randomnumber = uint(keccak256(abi.encodePacked(msg.sender, nonce)));
-        randomnumber = (randomnumber % 900) + 100;
-        uint8 playerPosX = uint8(randomnumber) % 3;
-        uint playerPosY = (randomnumber % 3);
+    function _generateRandomXYPoint() public returns (uint8) {
+        uint aNumber = uint(keccak256(abi.encodePacked(msg.sender, nonce)));
+        aNumber = (aNumber % 900) + 100;
+        uint8 pos = uint8(aNumber) % 9;
 
-        return (playerPosX, uint8(playerPosY));
+        return pos;
     }
 
-    function isPlannedMoveLegal(uint8 _x, uint8 _y) public returns (bool) {
-        for (uint8 row = 0; row < gameBoard.length; row++) {
-            for (uint col = 0; col < gameBoard.length; col++) {
-                if (row == _x && col == _y) {
-                    return gameBoard[row][col] == 10;
-                }
-            }
-        }
-        return false;
+    function isMoveLegal(uint8 _pos) public returns (bool) {
+        return gameBoard[_pos] == 10;
     }
 
 
@@ -182,4 +168,6 @@ contract TicTacToe is GameEvent {
 
         return "x";
     }
+
+
 }
--- test/TicTacToe.t.sol
+++ test/TicTacToe.t.sol
@@ -1,11 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Test, console} from "forge-std/Test.sol";
+import {Test, console, stdStorage, StdStorage} from "forge-std/Test.sol";
 import {TicTacToe} from "../src/TicTacToe.sol";
 import "../src/TicTacToe.sol";
 
 contract TicTacToeTest is Test {
+    using stdStorage for StdStorage;
+
     TicTacToe public sut;
 
     event GameStateEvent(string message);
@@ -14,13 +16,13 @@ contract TicTacToeTest is Test {
 
     function setUp() public {
         sut = new TicTacToe();
+        sut.initGameBoard();
     }
 
-    function xtest_initGameBoard() public {
+    function test_initGameBoard() public {
         vm.expectEmit();
         emit GameStateEvent("new game board created");
         sut.initGameBoard();
-        assertEq(sut.gameBoard(0, 0), 10);
     }
 
     function xtest_newGameEvent() public {
@@ -29,25 +31,34 @@ contract TicTacToeTest is Test {
         sut.initGameBoard();
     }
 
-    function xtest_makeMove() public {
-        vm.expectEmit();
-        emit MoveMadeEvent("X moved to 1, 1");
-        sut.makePlayerXMove(1, 1, 'nonce');
-        uint8 x = sut.gameBoard(1, 1);
-        assertEq(x, 1);
+
+    function test_makePlayerXMove() public {
+        sut.makePlayerMove(5, "fake nonce");
     }
 
     function xtest_makeBotMove() public {
         console.log(sut.nonce());
     }
 
+    function test_findColumnWinner() public {
+        sut.makePlayerMove(1, "1111");
+        sut.makePlayerMove(4, "1111");
+        sut.makePlayerMove(7, "1111");
+
+        bool actual = sut._findColumnWinner();
+
+        assertTrue(actual);
+    }
+
+    function test_findRowWinner() public {
+    }
 
     function xtest_isPlannedMoveLegal() public {
         vm.pauseGasMetering();
-        sut.makePlayerXMove(2, 1, 'nonce');
+        sut.makePlayerMove(2, 'nonce');
 
-        bool actual1 = sut.isPlannedMoveLegal(2, 1);
-        bool actual2 = sut.isPlannedMoveLegal(0, 0);
+        bool actual1 = sut.isMoveLegal(2);
+        bool actual2 = sut.isMoveLegal(0);
 
         assertEq(actual1, false);
         assertEq(actual2, true);
@@ -56,20 +67,20 @@ contract TicTacToeTest is Test {
     }
 
     function xtest_checkRowsForWinner() public {
-        sut.makePlayerXMove(0, 0, 'nonce');
-        sut.makePlayerXMove(0, 1, 'nonce');
-        sut.makePlayerXMove(0, 2, 'nonce');
-
-        bool actual = sut._checkBoardForWinner();
-        assertTrue(actual);
+//        sut.makePlayerXMove(0, 0, 'nonce');
+//        sut.makePlayerXMove(0, 1, 'nonce');
+//        sut.makePlayerXMove(0, 2, 'nonce');
+//
+//        bool actual = sut._checkBoardForWinner();
+//        assertTrue(actual);
     }
 
-    function test_checkColumnsForWinner() public {
-        sut.makePlayerXMove(0, 1, 'nonce');
-        sut.makePlayerXMove(1, 1, 'nonce');
-        sut.makePlayerXMove(2, 1, 'nonce');
-
-        bool actual = sut._checkBoardForWinner();
-        assertTrue(actual);
+    function xtest_checkColumnsForWinner() public {
+//        sut.makePlayerXMove(0, 1, 'nonce');
+//        sut.makePlayerXMove(1, 1, 'nonce');
+//        sut.makePlayerXMove(2, 1, 'nonce');
+//
+//        bool actual = sut._checkBoardForWinner();
+//        assertTrue(actual);
     }
 }
